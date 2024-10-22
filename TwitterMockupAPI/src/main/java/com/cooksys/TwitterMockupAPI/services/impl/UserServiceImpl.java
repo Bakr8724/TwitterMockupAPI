3b6d846c540a1e1f4611551a524ac2d08228ae6d
Lewis Phaosada <lewisphaosada@gmail.com>
2024-10-21 23:40:12 -0500

    Userservice impl class tweaked just a bit

--- TwitterMockupAPI/src/main/java/com/cooksys/TwitterMockupAPI/services/impl/UserServiceImpl.java
+++ TwitterMockupAPI/src/main/java/com/cooksys/TwitterMockupAPI/services/impl/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.cooksys.TwitterMockupAPI.services.impl;
 
 import com.cooksys.TwitterMockupAPI.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 }
