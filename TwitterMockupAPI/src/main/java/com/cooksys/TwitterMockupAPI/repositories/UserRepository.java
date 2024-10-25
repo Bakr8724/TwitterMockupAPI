b073c8537c6cadabd632a90bb67be98abb02efe0
Lewis Phaosada <lewisphaosada@gmail.com>
2024-10-25 13:43:43 -0500

    Testing

--- TwitterMockupAPI/src/main/java/com/cooksys/TwitterMockupAPI/repositories/UserRepository.java
+++ TwitterMockupAPI/src/main/java/com/cooksys/TwitterMockupAPI/repositories/UserRepository.java
@@ -21,7 +21,7 @@ public interface UserRepository extends JpaRepository<User,Long>{
     @Query("Select u from User u where u.deleted = false")
     List<User> getActiveUsers();
 
-//    List<User> findByDeletedFalse();
+    List<User> findByDeletedFalse();
 
     boolean existsByCredentialsUsername(String username);
 
